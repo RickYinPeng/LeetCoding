d8b9c58f9c7b12eb265843cdf7dbc5591f9b5ec1
RickYinPeng <272940172@qq.com>
2020-04-13 23:35:24 +0800

    今天被鸡蛋掉落砸晕了，355明天再看吧

---
+++ src/main/java/com/rickyin/coding/heap/coding_355_设计推特_未完成.java
@@ -0,0 +1,7 @@
+package com.rickyin.coding.heap;
+
+public class coding_355_设计推特_未完成 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/rickyin/coding/数学问题/coding_16_03_交点_未完成.java
+++ src/main/java/com/rickyin/coding/数学问题/coding_16_03_交点_未完成.java
@@ -1,6 +1,6 @@
 package com.rickyin.coding.数学问题;
 
-public class coding_16_03_交点 {
+public class coding_16_03_交点_未完成 {
     public static void main(String[] args) {
         
     }
